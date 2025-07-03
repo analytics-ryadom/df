57558da2d80ae7323d7dee0c99bf9e74d0f78850
Nikolay Ognev <ognev.n@ryadom.kz>
2025-07-03 05:22:03 +0000

    add mv_couriers_state

---
+++ definitions/sources.js
@@ -0,0 +1,31 @@
+declare({
+    database: "organic-reef-315010",
+    schema: "snp",
+    name: "orders",
+  });
+
+declare({
+    database: "organic-reef-315010",
+    schema: "snp",
+    name: "order_status_history",
+  });
+
+  declare({
+    database: "organic-reef-315010",
+    schema: "snp",
+    name: "shift_reasons",
+  });
+
+    declare({
+    database: "organic-reef-315010",
+    schema: "snp",
+    name: "employee_shift_status_history",
+  });
+
+      declare({
+    database: "organic-reef-315010",
+    schema: "snp",
+    name: "shifts",
+  });
+
+  
