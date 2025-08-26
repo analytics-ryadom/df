23079f4a8d7bbc8813807fb474cdeb067c555d7d
Nikolay Ognev <ognev.n@ryadom.kz>
2025-08-26 11:56:38 +0000

    Add Courier, Surge and preorders stg_*

--- includes/table_docs.js
+++ includes/table_docs.js
@@ -1,4 +1,4 @@
-
+/////////////////////////////// Lavka ///////////////////////////////
 const stg_lavka__warehouses = {
    additional_phone_number: "A second phone number of warehouse",
    additional_properties: "Contane some properities in json-structure",
@@ -69,12 +69,127 @@ const stg_lavka__additional_order_items = {
     status: "Item status",
     launch_id: "Launch or version ID"
 
+};
+
+const stg_lavka__additional_products = {
+    created_at: "Timestamp when this row added to table",
+    id: "Unique record ID",
+    is_active: "Indicates whether the ones is currently active",
+    name: "Additional product name",
+    type: "Additional product type",
+    updated_at: "Timestamp when this row was updated",
+    uuid: "Universally unique identifier for the Additional product (1C feature)",
+    launch_id: "Launch or version ID"
+
+};
+
+const stg_lavka__preorders = {
+  address_comment: "Address comment",
+  created_at: "Preorder creation timestamp",
+  customer_last_name: "Customer’s last name",
+  customer_name: "Customer’s first name",
+  customer_phone_number: "Customer phone number",
+  delivery_type: "Delivery type",
+  device_id: "Device ID from which the preorder was made",
+  entrance: "Entrance number",
+  flat_number: "Apartment number",
+  floor: "Floor",
+  hash: "Hash for uniqueness or identification",
+  id: "Unique record ID",
+  idempotency_key: "Idempotency key to prevent duplication",
+  lat: "Latitude of the delivery address",
+  leave_at_the_door: "Leave at the door flag",
+  less_packages: "Less packeges, please!",
+  long: "Longitude of the delivery address",
+  paid_at: "Timestamp of payment",
+  payment_deeplink: "Payment deeplink URL",
+  promocode_id: "ID of the applied promo code",
+  street: "Delivery street",
+  user_id: "ID of the user who made the preorder",
+  user_iin: "Users IIN (individual identification number)",
+  warehouse_id: "Warehouse (dark store) ID",
+  launch_id: "Launch or version ID",
+  free_delivery: "Indicates free delivery",
+  customer_address_id: "ID of customer’s saved address",
+  comment: "General preorder comment",
+  payment_id: "Payment ID in external payment system",
+  provider_user_id: "External provider user ID"
+    
+
+};
+
+/////////////////////////////// Courier ///////////////////////////////
+const stg_courier__courier_motivations = {
+    amount: "Мoney for motivation",
+    courier_id: "Unique courier id",
+    created_at: "Timestamp when this row added to table",
+    employee_id: "Unique employee id",
+    employee_rate_id: "An employees rate id",
+    got_bonus: "Indicates that courier gets bonuses",
+    got_bonus_turned_on: "Indicates that got_bonus feature is turner on or not",
+    id: "Unique record ID",
+    is_intern: "This employee is intern ",
+    motivation_program_id: "Unique motivation program id",
+    order_id: "An order delivered by courier in this case",
+    paid_amount: "How much paid for this motivation",
+    polygon_id: "Polygon id where it was",
+    shift_id: "Couriers shift id",
+    surge_bonus: "Bonuses by surge",
+    surge_coefficient: "Surge coefficient",
+    launch_id: "Launch or version ID",
+    rider_coefficient: "Riders coefficient",
+    polygon_data: "JSON contanes a polygon data",
+    paid_rider_coefficient: "The coefficient that was applied to the payment"
+
+};
+
+const stg_courier__motivation_programs = {
+    amount: "Мoney for motivation",
+    bonus_type: "Bonus type",
+    created_at: "Timestamp when this row added to table",
+    created_by: "Who added a motivation program",
+    employee_rate_id: "An employees rate id",
+    from_eta: "Lower eta range limit",
+    from_radius: "Lower radius range limit",
+    id: "Unique record ID",
+    is_active: "Indicates whether the ones is currently active",
+    rider_x_coefficient: "For RiderX coefficient",
+    surge_bonuses: "Bonuses by surge",
+    time: "Еarget time",
+    to_eta: "Upper eta range limit",
+    to_radius: "Upper radius range limit",
+    updated_at: "Timestamp when this row was updated",
+    updated_by: "Who udated a motivation program",
+    warehouse_id: "Unique identifier for the warehouse",
+    launch_id: "Launch or version ID"
+
+};
+
+/////////////////////////////// Lavka_surge ///////////////////////////////
+const stg_lavka_surge__surge_logs = {
+    additional_data: "JSON contanes a surge decision data",
+    decision: "Indicates that the surge is on",
+    decision_made_at: "Datetime of surge decision",
+    id: "ID of surge decision",
+    min_amount: "The minimum basket price set by surge",
+    min_amount_enabled: "The minimum basket price is enable",
+    next_decision_at: "Datetime until which this decision is valid",
+    request_uuid: "Universally unique identifier for surge decision",
+    test_mode: "It's test decision",
+    warehouse_id: "The warehouse where this decision operates",
+    launch_id: "Launch or version ID"
+
 }
-
-
 
 module.exports = {
   stg_lavka__warehouses,
   stg_lavka__order_items,
-  stg_lavka__additional_order_items
+  stg_lavka__additional_order_items,
+  stg_lavka__additional_products,
+  stg_lavka__preorders,
+  stg_courier__courier_motivations,
+  stg_courier__motivation_programs,
+  stg_lavka_surge__surge_logs
 };
+
+
